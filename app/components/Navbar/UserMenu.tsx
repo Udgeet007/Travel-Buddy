0695db17be63c30619d085640e38934ca6258313
Udgeet <bhatt8udgeet@gmail.com>
2024-04-20 19:30:27 +0530

    project

---
+++ app/components/Navbar/UserMenu.tsx
@@ -0,0 +1,151 @@
+'use client';
+
+import { useCallback, useState } from "react";
+import { AiOutlineMenu } from "react-icons/ai";
+import { signOut } from "next-auth/react";
+import { useRouter } from "next/navigation";
+
+import useLoginModal from "@/app/hooks/useLoginModal";
+import useRegisterModal from "@/app/hooks/useRegisterModal";
+import useRentModal from "@/app/hooks/useRentModal";
+import { SafeUser } from "@/app/types";
+
+import MenuItem from "./MenuItem";
+import Avatar from "../Avatar";
+
+interface UserMenuProps {
+  currentUser?: SafeUser | null
+}
+
+const UserMenu: React.FC<UserMenuProps> = ({
+  currentUser
+}) => {
+  const router = useRouter();
+
+  const loginModal = useLoginModal();
+  const registerModal = useRegisterModal();
+  const rentModal = useRentModal();
+
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleOpen = useCallback(() => {
+    setIsOpen((value) => !value);
+  }, []);
+
+  const onRent = useCallback(() => {
+    if (!currentUser) {
+      return loginModal.onOpen();
+    }
+
+    rentModal.onOpen();
+  }, [loginModal, rentModal, currentUser]);
+
+  return ( 
+    <div className="relative">
+      <div className="flex flex-row items-center gap-3">
+        <div 
+          onClick={onRent}
+          className="
+            hidden
+            md:block
+            text-md 
+            font-semibold 
+            py-3 
+            px-4 
+            rounded-full 
+            hover:bg-cyan-500 
+            transition 
+            cursor-pointer
+            
+          "
+        >
+        Add Your Home
+        </div>
+        <div 
+        onClick={toggleOpen}
+        className="
+          p-4
+          md:py-1
+          md:px-2
+          border-[1px] 
+          border-neutral-200 
+          flex 
+          flex-row 
+          items-center 
+          gap-3 
+          rounded-full 
+          cursor-pointer 
+          hover:bg-cyan-500 
+          transition
+          "
+        >
+          <AiOutlineMenu />
+          <div className="hidden md:block">
+            <Avatar src={currentUser?.image} />
+          </div>
+        </div>
+      </div>
+      {isOpen && (
+        <div 
+          className="
+            absolute 
+            rounded-xl 
+            shadow-md
+            w-[40vw]
+            md:w-3/4 
+            bg-white 
+            overflow-hidden 
+            right-0 
+            top-12 
+            text-md
+          "
+        >
+          <div className="flex flex-col cursor-pointer">
+            {currentUser ? (
+              <>
+                <MenuItem 
+                  label="My Trips" 
+                  onClick={() => router.push('/trips')}
+                />
+                <MenuItem 
+                  label="My Favorites" 
+                  onClick={() => router.push('/favorites')}
+                />
+                <MenuItem 
+                  label="My Reservations" 
+                  onClick={() => router.push('/reservations')}
+                />
+                <MenuItem 
+                  label="My Properties" 
+                  onClick={() => router.push('/properties')}
+                />
+                <MenuItem 
+                  label="Add Your House" 
+                  onClick={rentModal.onOpen}
+                />
+                <hr />
+                <MenuItem 
+                  label="Logout" 
+                  onClick={() => signOut()}
+                />
+              </>
+            ) : (
+              <>
+                <MenuItem 
+                  label="Login" 
+                  onClick={loginModal.onOpen}
+                />
+                <MenuItem 
+                  label="Sign up" 
+                  onClick={registerModal.onOpen}
+                />
+              </>
+            )}
+          </div>
+        </div>
+      )}
+    </div>
+   );
+}
+ 
+export default UserMenu;
---
+++ app/layout.tsx
@@ -0,0 +1,51 @@
+import type { Metadata } from 'next'
+import { Nunito } from 'next/font/google'
+import './globals.css'
+import Navbar from './components/Navbar/Navbar'
+
+import ClientOnly from './components/ClientOnly'
+import RegisterModal from './components/Modals/RegisterModal'
+import ToasterProvider from './providers/ToasterProvider'
+import LoginModal from './components/Modals/LoginModal'
+import getCurrentUser from './actions/getCurrentUser'
+import RentModal from './components/Modals/RentModal'
+import SearchModal from './components/Modals/SearchModal'
+import ViewSwitchBadge from './components/ViewSwitchBadge'
+
+
+const inter = Nunito({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'Travel-Buddy',
+  description: 'Travel Website',
+}
+
+const font = Nunito({
+  subsets: ['latin'],
+});
+
+export default async function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  const currentUser = await getCurrentUser();
+  return (
+    <html lang="en">
+      <body className={font.className}>
+        <ClientOnly>
+          <ToasterProvider/>
+          <LoginModal/>
+          <SearchModal/>
+          <RentModal />
+          
+          <RegisterModal/>
+          <Navbar currentUser ={currentUser}/>
+        </ClientOnly>
+        <div className='pb-20 pt-28 '>
+        {children}
+        </div>
+      </body>
+    </html>
+  )
+}
